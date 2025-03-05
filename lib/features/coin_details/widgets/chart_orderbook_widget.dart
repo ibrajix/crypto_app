eab9bac61a8d4987f34c2cadc32bf1475057bb3f
Ibrahim <ibrajix@gmail.com>
2025-03-05 10:03:13 +0100

    tab widget for charts, orderbook and recent trades

--- lib/features/coin_details/widgets/chart_orderbook_widget.dart
+++ lib/features/coin_details/widgets/chart_orderbook_widget.dart
@@ -32,41 +32,68 @@ class _ChartsOrderbookWidgetState extends ConsumerState<ChartsOrderbookWidget>
   Widget build(BuildContext context) {
     final isDarkMode = ref.watch(themeProvider) == ThemeMode.dark;
 
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: isDarkMode ? greyBg : whiteShade,
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-      ),
-      child: TabBar(
-        indicatorWeight: 0,
-        indicatorPadding: EdgeInsets.all(0),
-        indicatorSize: TabBarIndicatorSize.tab,
-        controller: _tabController,
-        indicator: BoxDecoration(
-          borderRadius: BorderRadius.circular(
-            16.0,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 9),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: isDarkMode ? greyBg : whiteShade,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+            child: TabBar(
+              dividerColor: Colors.transparent,
+              indicatorWeight: 0,
+              indicatorPadding: EdgeInsets.all(0),
+              indicatorSize: TabBarIndicatorSize.tab,
+              controller: _tabController,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  16.0,
+                ),
+                color: isDarkMode ? greyBgShade.withOpacity(0.05) : white,
+              ),
+              labelStyle: TextStyle(
+                color: isDarkMode ? white : greyBg,
+                fontSize: 13,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Satoshi',
+              ),
+              unselectedLabelStyle: TextStyle(
+                color: isDarkMode ? white : blackShade,
+                fontSize: 13,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Satoshi',
+              ),
+              splashBorderRadius: BorderRadius.circular(20),
+              padding: const EdgeInsets.all(3),
+              tabs: const [
+                Tab(text: 'Charts'),
+                Tab(text: 'Orderbook'),
+                Tab(text: 'Recent trades'),
+              ],
+            ),
           ),
-          color: isDarkMode ? greyBgShade.withOpacity(0.05) : white,
-        ),
-        labelStyle: TextStyle(
-          color: isDarkMode ? white : greyBg,
-          fontSize: 13,
-          fontWeight: FontWeight.w600,
-          fontFamily: 'Satoshi',
-        ),
-        unselectedLabelStyle: TextStyle(
-          color: isDarkMode ? white : blackShade,
-          fontSize: 13,
-          fontWeight: FontWeight.w600,
-          fontFamily: 'Satoshi',
-        ),
-        splashBorderRadius: BorderRadius.circular(20),
-        padding: const EdgeInsets.all(3),
-        tabs: const [
-          Tab(text: 'Charts'),
-          Tab(text: 'Orderbook'),
-          Tab(text: 'Recent trades'),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            decoration: BoxDecoration(
+              color: isDarkMode ? cardColorDark : white,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+            child: SizedBox(
+              height: 300,
+              child: TabBarView(
+                controller: _tabController,
+                children: const [
+                  Text("Charts Content"),
+                  Text("Orderbook Content"),
+                  Text("Recent Trades Content"),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
